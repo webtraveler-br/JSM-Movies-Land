90dfd8ba2c752408e3d7e028e90610f38b87707a
Lucas Prestes Mendes <mlucasprestes@gmail.com>
2022-05-23 07:36:36 -0300

    :sparkles: componente MovieCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 
+import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
 
@@ -39,27 +40,7 @@ const App = () => {
       </div>
 
       <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie.Year}</p>
-          </div>
-
-          <div>
-            <img
-              src={
-                movie.Poster !== "N/A"
-                  ? movie.Poster
-                  : "https://via.placeholder.com/400"
-              }
-              alt={movie.Title}
-            />
-          </div>
-
-          <div>
-            <span>{movie.Type}</span>
-            <h3>{movie.Title}</h3>
-          </div>
-        </div>
+        <MovieCard movie={movie} />
       </div>
     </div>
   );
